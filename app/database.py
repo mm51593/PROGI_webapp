1f5dabc2b86916cfb2a09a2b21ceb97d8a104f3c
Maksimilijan Marošević <mm51593@fer.hr>
2020-10-24 00:28:42 +0200

    Restructured User table

--- app/database.py
+++ app/database.py
@@ -3,9 +3,10 @@ from app import application
 db = SQLAlchemy(application)
 
 class User(db.Model):
-    username = db.Column(db.String(20), unique=True, nullable=False, primary_key=True)
+    userID = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(50), nullable=False)
 
     def __repr__(self):     # output of print()
-        return f"User('{self.username}', '{self.email}', '{self.password}')"
+        return f"User('{self.userID}', '{self.username}', '{self.email}', '{self.password}')"
