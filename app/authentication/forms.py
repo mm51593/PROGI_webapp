6c2769d3d5c349e18d4f6cd37b037db3c6e4335a
Maksimilijan Marošević <mm51593@fer.hr>
2020-10-24 02:32:20 +0200

    Added login form

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -1,11 +1,29 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.database import User
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=4, max=25)])
+    username = StringField('Korisničko ime', validators=[DataRequired(), Length(min=4, max=25)])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    confirmPassword = PasswordField('Confirm password', validators=[DataRequired(), EqualTo(password)])
-    submit = SubmitField('Sign up')
+    password = PasswordField('Lozinka', validators=[DataRequired()])
+    confirmPassword = PasswordField('Potvrdi lozinku', validators=[DataRequired(), EqualTo(password)])
+    submit = SubmitField('Registriraj me')
+
+    def validate_username(self, username):
+        fromdb = User.query.filter_by(username = username.data).first()
+        if fromdb:
+            raise ValidationError('Ovo korisničko ime je zauzeto')
+        return
+
+    def validate_email(self, email):
+        fromdb = User.query.filter_by(email = email.data).first()
+        if fromdb:
+            raise ValidationError('Račun s ovom email adresom već postoji.')
+        return
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Lozinka', validators=[DataRequired()])
+    remember = BooleanField('Zapamti me')
+    submit = SubmitField('Prijava')
