8b0f8744ae55750a6e0cb3eb181b5e95e8442166
Maksimilijan Marošević <mm51593@fer.hr>
2020-11-03 23:29:11 +0100

    Dodane povratne informacije pri registraciji

--- app/authentication/forms.py
+++ app/authentication/forms.py
@@ -5,16 +5,16 @@ from app.database import User
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField('Korisničko ime', validators=[DataRequired(), Length(min=3, max=25)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Lozinka', validators=[DataRequired()])
-    confirm_password = PasswordField('Potvrdi lozinku', validators=[DataRequired(), EqualTo('password')])
+    username = StringField('Korisničko ime', validators=[DataRequired("Ovo polje je neophodno."), Length(min=3, max=25, message="Korisničko ime mora biti između %(min)d i %(max)d znakova dugo.")])
+    email = StringField('Email', validators=[DataRequired("Ovo polje je neophodno."), Email(message="Pogrešan format E-mail adrese.")])
+    password = PasswordField('Lozinka', validators=[DataRequired("Ovo polje je neophodno."), Length(min=8, message="Lozinka mora biti barem %(min)d znakova duga.")])
+    confirm_password = PasswordField('Potvrdi lozinku', validators=[DataRequired("Ovo polje je neophodno."), EqualTo('password', message="Lozinke se ne podudaraju.")])
     submit = SubmitField('Registriraj me')
 
     def validate_username(self, username):
         fromdb = User.query.filter_by(username=username.data).first()
         if fromdb:
-            raise ValidationError('Ovo korisničko ime je zauzeto')
+            raise ValidationError('Ovo korisničko ime je zauzeto.')
         return
 
     def validate_email(self, email):
