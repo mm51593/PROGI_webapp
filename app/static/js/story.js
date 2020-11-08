f9d6df04444ae69466816583eb6b96dbcc22bbea
Valentin Jukanović <valentin.jukanovic@fer.hr>
2020-11-08 15:02:16 +0100

    Dodan CSS i JS za storyrequest

--- app/static/js/story.js
+++ app/static/js/story.js
@@ -1,17 +1,24 @@
 function removeElement(self)
 {
-    self.parentElement.remove()
+    self.parentElement.parentElement.remove()
     document.getElementById("input-number").value--;
 }
-
+ 
 function reevaluateNames()
 {
     var list = document.getElementById("InputList").childNodes;
     var index = 1;
 
     for (var item of list)
-    {
-        item.getElementsByTagName("input")[0].name = item.inputtype + "-" + index.toString();
+    {   
+        var searchHelp = item.childNodes;
+        searchHelp = searchHelp[0].childNodes;
+
+        if(searchHelp[0].getElementsByTagName("input")[0] == undefined) {
+            searchHelp[0].getElementsByTagName("textarea")[0].name = item.inputtype + "-" + index.toString();
+        } else {
+            searchHelp[0].getElementsByTagName("input")[0].name = item.inputtype + "-" + index.toString();
+        }
         index++;
     }
 }
@@ -21,37 +28,56 @@ function addTextField(type)
 {
     var newlistitem = document.createElement("li");
 
-    var field = document.createElement("input");
+    var field;
 
     if (type == "text")
     {
+        field = document.createElement("textarea");
         newlistitem.inputtype = "text";
         field.type = "text";
+        field.className = "text-field-class";
     }
     else if (type == "image")
     {
+        field = document.createElement("input");
         newlistitem.inputtype = "image";
         field.type = "file";
         field.accept = "image/*";
     }
     else
     {
+        field = document.createElement("input");
         newlistitem.inputtype = "video";
         field.type = "file";
         field.accept = "video/*";
     }
 
+    var listDiv = document.createElement("div");
+    listDiv.className = "listDiv";
+
+    var contentDiv = document.createElement("div");
+    contentDiv.className = "div-content";
+
     var newlabel = document.createElement("label");
     newlabel.textContent = "Add " + type;
+    newlabel.className = "elem-submit-" + type;
 
     var removebutton = document.createElement("button");
     removebutton.type = "button";
     removebutton.textContent = "Remove";
+    removebutton.className = "rmv-prijedlog-elem";
     removebutton.onclick = function() {return removeElement(removebutton);};
 
-    newlistitem.append(newlabel);
-    newlistitem.append(field);
-    newlistitem.append(removebutton);
+    contentDiv.append(newlabel);
+    contentDiv.append(field);
+   
+    listDiv.append(contentDiv);
+    listDiv.append(removebutton);
+    
+    newlistitem.append(listDiv);
+    newlistitem.className = "elem-liste";
+    
+    
 
     document.getElementById("input-number").value++;
     document.getElementById("InputList").append(newlistitem);
