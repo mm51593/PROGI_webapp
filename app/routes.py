fe5d556b2465244d0bb2453e8ded8e821f532ccc
Luka Cigula <luka.cigula@fer.hr>
2020-11-08 18:31:07 +0000

    routes.py ažuriran sa index pageom

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import application
+from flask import Flask, render_template, url_for
 
 @application.route('/')
 @application.route('/index')
 def index():
-    return "Hello world"
+    return render_template('index.html', title='Početna')
