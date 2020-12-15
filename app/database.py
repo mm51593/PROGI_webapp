d2a717ecca6c34f2d9bd7f03c51a2d5766f70bb3
Bojan Antunović <ba51666@fer.hr>
2020-12-15 04:34:34 +0100

    Dodani ModelPhoto, ModelPrice, OrderModel

--- app/database.py
+++ app/database.py
@@ -20,23 +20,48 @@ class User(db.Model, UserMixin):
     def __repr__(self):     # output of print()
         return f"User('{self.id}', '{self.username}', '{self.email}', '{self.password}')"
 
-class Product(db.Model):
-    id_p = db.Column(db.Integer, primary_key=True)
+class Model(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(60), unique=True, nullable=False)
     description = db.Column(db.String(500), nullable=True)          # nullable could later be omitted, description size = 500 for now
-    price = db.Column(db.Integer, nullable=False)                   # need to add restriction for price>0
-    image_file = db.Column(db.String(20), nullable=False, default='maketaimg.jpg')      # 'maketaimg.jpg' as current placeholder
-    material = db.Column(db.String(20))                             # default value could be added, material to be implemented as static type?, nullable could be implemeted
+    creator_id = db.Column(db.Integer, db.Foreign_Key('user.id') , nullable=False)
 
     def __repr__(self):     # output of print()
-        return f"Product('{self.id_p}', '{self.name}', '{self.description}', '{self.price}', '{self.image_file}', '{self.material}')"
+          return f"Model('{self.id}', '{self.name}', '{self.description}', '{self.creator_id}')"
 
-products = {
-        'name': 'Maketa 1',
-        'description': 'blablabla bla',
-        'price': '100',
-        'image_file': '',
-        'material': ''
-    }
+class ModelPhoto(db.Model):
+    image_name = db.Column(db.String(50), primary_key=True, nullable=False)  # 'maketaimg.jpg'
+    model_id = db.Column(db.Integer, db.Foreign_Key('model.id'), nullable=False)
+
+    def __repr__(self):     # output of print()
+            return f"ModelPhoto('{self.image_name}', '{self.model_id}')"
+
+class ModelPrice(db.Model): 
+    model_id = db.Column(db.Integer, db.Foreign_Key('model.id'), primary_key=True) 
+    material = db.Column(db.String(20), nullable=False)    # composite key?, static type for material?
+    price = db.Column(db.Integer, nullable=False)    # need to add restriction for price>0    
+
+    def __repr__(self):     # output of print()
+          return f"ModelPrice('{self.model_id}', '{self.material}', '{self.price}')" 
+                             
+
+class OrderModel(db.Model):
+    order_id = db.Column(db.Integer, db.Foreign_Key('order.id'), nullable=False)
+    model_id = db.Column(db.Integer, db.Foreign_Key('model.id'), nullable=False)
+    material = db.Column(db.String(20), nullable=False)
+    price = db.Column(db.Integer, nullable=False)  # numeric type instead of integer?
+
+    def __repr__(self):     # output of print()
+        return f"OrderModel('{self.order_id}', '{self.model_id}', '{self.material}', '{self.price}')"
+
+
+
+#products = {
+#        'name': 'Maketa 1',
+#        'description': 'blablabla bla',
+#        'price': '100',
+#        'image_file': '',
+#        'material': ''
+#    }
 
     
