79ed0becaa89b4be0406fcdd6696666ba6ae2e5c
FER\vj51523 <valentin.jukanovic@fer.hr>
2020-12-16 21:36:56 +0100

    Prihvat cijene/price

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -15,6 +15,10 @@ function checkUpload() {
     }
 }
 
+function rejectOffer(offerNum) {
+    let offer = document.getElementById("ponuda/cijena-" + offerNum).remove();
+}
+
 function checkData() {
     if(document.getElementById("upload-vid").value == "" && document.getElementById("upload-img").value == "" && document.getElementById("opisPrice").value == "") {
         document.getElementById("tekstLabel").innerHTML = "Tekst :   <span style='color: red;'>Trebate dati barem jedno od teksta, slike ili videa</span>";
