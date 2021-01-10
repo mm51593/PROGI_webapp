4f372996e56a83cd13f47097f10c7e23ce960bd4
Valentin Jukanović <valentin.jukanovic@fer.hr>
2021-01-10 16:25:40 +0100

    Dodan prikaz greske

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,18 +1,32 @@
 function checkUpload() {
     var img = document.getElementById("imgLabel");
     var vid = document.getElementById("vidLabel");
-    
-    if(document.getElementById("upload-img").value != "") {
+
+    if (document.getElementById("upload-img").value != "") {
         img.style.backgroundColor = "lawngreen";
     } else {
         img.style.backgroundColor = "#ff5500"
     }
-    
-    if(document.getElementById("upload-vid").value != "") {
+
+    if (document.getElementById("upload-vid").value != "") {
         vid.style.backgroundColor = "lawngreen";
     } else {
         vid.style.backgroundColor = "#ff5500"
     }
+}
+
+function appendError(error) {
+    console.log(error);
+    var errMsg = document.getElementsByClassName("regErrMsg")[0];
+
+    var errReq = document.createElement("span");
+    errReq.innerHTML = error;
+
+    errMsg.appendChild(errReq);
+
+    errMsg.style.marginTop = "20px";
+    errMsg.style.padding = "10px";
+    errMsg.style.opacity = "1";
 }
 
 function checkCijena(element) {
@@ -21,10 +35,10 @@ function checkCijena(element) {
 
     restoreReg();
 
-    if(document.getElementById(element).value == "") {
+    if (document.getElementById(element).value == "") {
         error = true;
         var errReq = document.createElement("span");
-        errReq.innerHTML = "Cijena mora biti unesena";
+        errReq.innerHTML = "Cijena mora biti unesena.";
 
         errMsg.appendChild(errReq);
     }
@@ -32,16 +46,16 @@ function checkCijena(element) {
     var regCijena = new RegExp('^\\d+$');
     console.log(!regCijena.test(document.getElementById(element).value) + " za " + document.getElementById(element).value);
 
-    if(!document.getElementById(element).value == "") {
-        if(!regCijena.test(document.getElementById(element).value)) {
+    if (!document.getElementById(element).value == "") {
+        if (!regCijena.test(document.getElementById(element).value)) {
             error = true;
             var errReq = document.createElement("span");
-            errReq.innerHTML = "Samo brojevi smiju biti uneseni";
-    
+            errReq.innerHTML = "Samo brojevi smiju biti uneseni.";
+
             errMsg.appendChild(errReq);
         }
     }
-    
+
     if (error) {
         errMsg.style.marginTop = "20px";
         errMsg.style.padding = "10px";
@@ -58,11 +72,11 @@ function checkNarudzba() {
 
     restoreReg();
 
-    if(document.getElementById("materijaliMaketa").value == "" || document.getElementById("opisMaketa").value == ""
-       || document.getElementById("dimenzijeMaketa").value == "") {
+    if (document.getElementById("materijaliMaketa").value == "" || document.getElementById("opisMaketa").value == ""
+        || document.getElementById("dimenzijeMaketa").value == "") {
         error = true;
         var errReq = document.createElement("span");
-        errReq.innerHTML = "Sva polja moraju biti popunjena";
+        errReq.innerHTML = "Sva polja moraju biti popunjena.";
 
         errMsg.appendChild(errReq);
     }
@@ -74,7 +88,7 @@ function checkNarudzba() {
         return false;
     }
 
-    return true; 
+    return true;
 }
 
 function checkMaketa() {
@@ -83,10 +97,10 @@ function checkMaketa() {
 
     restoreReg();
 
-    if(document.getElementById("materijaliChoose").value == "noneSelected") {
+    if (document.getElementById("materijaliChoose").value == "noneSelected") {
         error = true;
         var errReq = document.createElement("span");
-        errReq.innerHTML = "Materijal mora biti odabran";
+        errReq.innerHTML = "Materijal mora biti odabran.";
 
         errMsg.appendChild(errReq);
     }
@@ -98,11 +112,11 @@ function checkMaketa() {
         return false;
     }
 
-    return true; 
+    return true;
 }
 
 function acceptOfferRed() {
-     location.replace('./prihvat_price_list.html');
+    location.replace('./prihvat_price_list.html');
 }
 
 function rejectOfferRed() {
@@ -113,16 +127,57 @@ function rejectOffer(offerNum) {
     let offer = document.getElementById("ponuda/cijena-" + offerNum).remove();
 }
 
+function checkLogin() {
+    var error = false;
+    var errMsg = document.getElementsByClassName("regErrMsg")[0];
+    var regMail = new RegExp("\\s*\\S+@\\S+.\\S+");
+
+    restoreReg();
+
+    if (document.getElementById("loginMail").value == "" || document.getElementById("loginLoz").value == "") {
+        error = true;
+        var errReq = document.createElement("span");
+        errReq.innerHTML = "Podaci moraju biti popunjeni.";
+
+        errMsg.appendChild(errReq);
+    } else {
+        if (!regMail.test(document.getElementById("loginMail").value)) {
+            error = true;
+            var errReq = document.createElement("span");
+            errReq.innerHTML = "Neispravan format email adrese.";
+
+            errMsg.appendChild(errReq);
+        }
+
+        if (document.getElementById("loginLoz").value.length < 8) {
+            error = true;
+            var errReq = document.createElement("span");
+            errReq.innerHTML = "Lozinka mora sadrzavati barem 8 znakova.";
+
+            errMsg.appendChild(errReq);
+        }
+    }
+
+    if (error) {
+        errMsg.style.marginTop = "20px";
+        errMsg.style.padding = "10px";
+        errMsg.style.opacity = "1";
+        return false;
+    }
+
+    return true;
+}
+
 function checkData() {
     var error = false;
     var errMsg = document.getElementsByClassName("regErrMsg")[0];
 
     restoreReg();
 
-    if(document.getElementById("prijedlogNaslovPrice").value == "") {
+    if (document.getElementById("prijedlogNaslovPrice").value == "") {
         error = true;
         var errReq = document.createElement("span");
-        errReq.innerHTML = "Mora biti upisan naslov";
+        errReq.innerHTML = "Mora biti upisan naslov.";
 
         errMsg.appendChild(errReq);
     }
@@ -130,7 +185,7 @@ function checkData() {
     if (document.getElementById("InputList").childNodes.length == 0) {
         error = true;
         var errReq = document.createElement("span");
-        errReq.innerHTML = "Mora biti dan barem jedan podatak";
+        errReq.innerHTML = "Mora biti dan barem jedan podatak.";
 
         errMsg.appendChild(errReq);
     }
@@ -139,22 +194,22 @@ function checkData() {
     var textArray = document.getElementsByClassName("text-field-class");
     var imgVidArray = document.getElementsByClassName("elem-check-selected");
 
-    for(var i = 0; i < textArray.length; i++) {
-        if(textArray[i].value == "") {
+    for (var i = 0; i < textArray.length; i++) {
+        if (textArray[i].value == "") {
             nonEnterElem = true;
         }
     }
 
-    for(var i = 0; i < imgVidArray.length; i++) {
-        if(imgVidArray[i].value == "") {
+    for (var i = 0; i < imgVidArray.length; i++) {
+        if (imgVidArray[i].value == "") {
             nonEnterElem = true;
         }
     }
 
-    if(nonEnterElem) {
+    if (nonEnterElem) {
         error = true;
         var errReq = document.createElement("span");
-        errReq.innerHTML = "Odabrani elementi moraju biti popunjeni";
+        errReq.innerHTML = "Odabrani elementi moraju biti popunjeni.";
 
         errMsg.appendChild(errReq);
     }
@@ -181,6 +236,15 @@ function checkPswd() {
 
     restoreReg();
 
+    if (pswd.length < 8) {
+        error = true;
+        var errPass = document.createElement("span");
+        errPass.innerHTML = "Lozinka mora sadrzavati barem 8 znakova.";
+
+        errMsg.appendChild(errPass);
+        console.log("Error logged, errMsg: " + errMsg);
+    }
+
     if (pswd != rePswd) {
         error = true;
         var errPass = document.createElement("span");
@@ -193,7 +257,7 @@ function checkPswd() {
     if (mail == "" || rePswd == "" || pswd == "" || user == "") {
         error = true;
         var errReq = document.createElement("span");
-        errReq.innerHTML = "Moraju biti popunjena sva polja";
+        errReq.innerHTML = "Moraju biti popunjena sva polja.";
 
         errMsg.appendChild(errReq);
     }
@@ -201,7 +265,7 @@ function checkPswd() {
     if (!regMail.test(mail)) {
         error = true;
         var errMail = document.createElement("span");
-        errMail.innerHTML = "E-mail je neispravnog formata";
+        errMail.innerHTML = "E-mail je neispravnog formata.";
 
         errMsg.appendChild(errMail);
     }
@@ -234,14 +298,11 @@ function updatePrice() {
     var selectValue = materialDropdown.value;
     var priceList = document.getElementById("prices");
 
-    for (var node of priceList.children)
-    {
-        if (node.getAttribute("value") == selectValue)
-        {
+    for (var node of priceList.children) {
+        if (node.getAttribute("value") == selectValue) {
             node.hidden = false;
         }
-        else
-        {
+        else {
             node.hidden = true;
         }
     }
