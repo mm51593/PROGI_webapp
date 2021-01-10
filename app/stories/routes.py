03e026e6b0b0b69acb45548e2dbf5221cae7a7e6
Mijo Piškur <mp51830@fer.hr>
2021-01-10 04:01:12 +0000

    Popravljen validation

--- app/stories/routes.py
+++ app/stories/routes.py
@@ -88,20 +88,22 @@ def pull_file(file):
     route = path.join(application.root_path, application.config['STORY_LOCATION'])
     return send_from_directory(route, file)
 
+
 @stories.route('/story/validation/<story_id>', methods=['GET', 'POST'])
 def validation(story_id):
     #TODO: osiguraj da samo admin moze na ovaj URL
     validationstory = Story.query.filter_by(id=story_id).first()
-
+    story_title = validationstory.title
+    story_elements = sorted(StoryContent.query.filter_by(story_id=story_id), key=lambda x: x.ordinal_number)
     if request.method == 'POST':
-        if request.form.get("Prihvati"):
-            validationstory.validated = "True"
+        if request.form['prihvPonudaPric'] == 'Prihvati':
+            validationstory.validated = True
             db.session.add(validationstory)
             db.session.commit()
-        elif request.form.get("Odbij"):
+        elif request.form.get['odbPonudaPric'] == 'Odbaci':
             db.session.delete(validationstory)
             db.session.commit()
-    return render_template("prihvat_price.html", title="Prihvat price")
+    return render_template("prihvat_price.html", title="Prihvat price", story_title=story_title, story_elements=story_elements, elem_len=len(story_elements))
 
 @stories.route('/stories')
 def display_story_list():
