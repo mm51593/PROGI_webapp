845c16b2ee53ed6dd020c9f69a954ab70b55d24f
Maksimilijan Marošević <mm51593@fer.hr>
2021-01-12 19:22:28 +0100

    bugfixing

--- app/checkout/routes.py
+++ app/checkout/routes.py
@@ -1,44 +1,44 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import current_user, login_required
 from app.checkout.forms import CheckoutForm
-from app.database import PodaciPlacanje
+from app.database import PodaciPlacanje, db
 
 checkout = Blueprint('checkout', __name__)
 
 @checkout.route('/checkout', methods=['GET','POST'])
 def Checkout():
     form = CheckoutForm()
+    billing_info = PodaciPlacanje.query.filter_by(id=current_user.id).first()
     if form.validate_on_submit():
         if current_user.is_authenticated:
-            profile = PodaciPlacanje.query.filter_by(id=current_user.id).first()
-            profile.Ime_Prezime = form.fname.data
-            profile.email = form.email.data
-            profile.Adresa = form.adr.data
-            profile.Grad = form.city.data
-            profile.Ime_Kartica = form.cname.data
-            profile.Broj_Kartica = form.ccnum.data
-            profile.Datum_Isteka = form.expmonth.data
-            profile.Drzava = form.state.data
-            profile.Postanski_Broj = form.zip.data
-            profile.Godina_Isteka = form.expyear.data
-            profile.CVV = form.cvv.data
+            billing_info.Ime_Prezime = form.fname.data
+            billing_info.email = form.email.data
+            billing_info.Adresa = form.adr.data
+            billing_info.Grad = form.city.data
+            billing_info.Ime_Kartica = form.cname.data
+            billing_info.Broj_Kartica = form.ccnum.data
+            billing_info.Datum_Isteka = form.expmonth.data
+            billing_info.Drzava = form.state.data
+            billing_info.Postanski_Broj = form.zip.data
+            billing_info.Godina_Isteka = form.expyear.data
+            billing_info.CVV = form.cvv.data
             db.session.commit()
         flash ('Uspješno obavljena kupnja', 'success')
         return redirect(url_for('/'))
     elif request.method == 'GET':
         if current_user.is_authenticated:
-            form.fname.data = profile.Ime_Prezime
-            form.email.data = profile.email
-            form.adr.data = profile.Adresa
-            form.city.data = profile.Grad
-            form.cname.data = profile.Ime_Kartica
-            form.ccnum.data = profile.Broj_Kartica
-            form.ccnum.data = profile.Datum_Isteka
-            form.expmonth.data = profile.Datum_Isteka
-            form.state.data = profile.Drzava
-            form.zip.data = profile.Postanski_Broj
-            form.expyear.data = profile.Godina_Isteka
-            form.cvv.data = profile.CVV
+            form.fname.data = billing_info.Ime_Prezime
+            form.email.data = billing_info.email
+            form.adr.data = billing_info.Adresa
+            form.city.data = billing_info.Grad
+            form.cname.data = billing_info.Ime_Kartica
+            form.ccnum.data = billing_info.Broj_Kartica
+            form.ccnum.data = billing_info.Datum_Isteka
+            form.expmonth.data = billing_info.Datum_Isteka
+            form.state.data = billing_info.Drzava
+            form.zip.data = billing_info.Postanski_Broj
+            form.expyear.data = billing_info.Godina_Isteka
+            form.cvv.data = billing_info.CVV
     else:
         print(form.errors)
     return render_template('checkout.html',title = "Podaci za placanje", form = form)
