38b2141e5f777560a702253ffe686d97855a3586
Maksimilijan Marošević <mm51593@fer.hr>
2021-01-13 21:30:01 +0100

    Popravljen bug gdje html form na checkout stranici nije povezan s backendom

--- app/checkout/forms.py
+++ app/checkout/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, DateField, SubmitField, IntegerField	
+from wtforms import StringField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length
 
 
@@ -10,8 +10,9 @@ class CheckoutForm(FlaskForm):
     city = StringField('Grad', validators=[DataRequired("Ovo polje je neophodno."), Length(min=3, max=20, message="Grad mora biti između %(min)d i %(max)d znakova dug.")])
     cname = StringField('Ime na kartici', validators=[DataRequired("Ovo polje je neophodno."), Length(min=6, max=40, message="Ime na kartici mora biti između %(min)d i %(max)d znakova dugo.")])
     ccnum = StringField('Broj na kartici', validators=[DataRequired("Ovo polje je neophodno."), Length(min=18, max=20, message="Broj na kartici mora biti između %(min)d i %(max)d znakova dugo.")])
-    expmonth = StringField('Datum isteka', validators=[DataRequired("Ovo polje je neophodno."), Length(min=6, max=10, message="Datum isteka mora biti između %(min)d i %(max)d znakova dug.")])
-    state = StringField('Drzava', validators=[DataRequired("Ovo polje je neophodno."), Length(min=3, max=20, message="Drava mora biti između %(min)d i %(max)d znakova duga.")])
-    zip = StringField('Postanski broj', validators=[DataRequired("Ovo polje je neophodno."), Length(min=3, max=20, message="Postanski Broj mora biti između %(min)d i %(max)d znakova dug.")])
+    expdate = StringField('Datum isteka', validators=[DataRequired("Ovo polje je neophodno."), Length(min=4, max=5, message="Datum isteka mora biti između %(min)d i %(max)d znakova dug.")])
+    state = StringField('Država', validators=[DataRequired("Ovo polje je neophodno."), Length(min=3, max=20, message="Drava mora biti između %(min)d i %(max)d znakova duga.")])
+    zip = StringField('Poštanski broj', validators=[DataRequired("Ovo polje je neophodno."), Length(min=3, max=20, message="Postanski Broj mora biti između %(min)d i %(max)d znakova dug.")])
     cvv = StringField('CVV', validators=[DataRequired("Ovo polje je neophodno."), Length(min=3, max=4, message="CVV mora biti između %(min)d i %(max)d znakova dug.")])
-    submit = SubmitField('Zavrsi s kupnjom')
+    remember = BooleanField('Zapamti podatke')
+    submit = SubmitField('Završi s kupnjom')
--- app/checkout/routes.py
+++ app/checkout/routes.py
@@ -9,7 +9,7 @@ checkout = Blueprint('checkout', __name__)
 def checkout_page():
     form = CheckoutForm()
     if form.validate_on_submit():
-        if current_user.is_authenticated:
+        if current_user.is_authenticated and form.remember:
             billing_info = PodaciPlacanje.query.filter_by(user_id=current_user.id).first()
             billing_info.full_name = form.fname.data
             billing_info.email = form.email.data
@@ -17,13 +17,13 @@ def checkout_page():
             billing_info.city = form.city.data
             billing_info.card_full_name = form.cname.data
             billing_info.card_number = form.ccnum.data
-            billing_info.card_expiry = form.expmonth.data
+            billing_info.card_expiry = form.expdate.data
             billing_info.country = form.state.data
             billing_info.zip_code = form.zip.data
             billing_info.card_CVC = form.cvv.data
             db.session.commit()
         flash ('Uspješno obavljena kupnja', 'success')
-        return redirect(url_for('/'))
+        return redirect(url_for('index.homepage'))
     elif request.method == 'GET':
         if current_user.is_authenticated:
             billing_info = PodaciPlacanje.query.filter_by(user_id=current_user.id).first()
@@ -33,7 +33,7 @@ def checkout_page():
             form.city.data = billing_info.city
             form.cname.data = billing_info.card_full_name
             form.ccnum.data = billing_info.card_number
-            form.expmonth.data = billing_info.card_expiry
+            form.expdate.data = billing_info.card_expiry
             form.state.data = billing_info.country
             form.zip.data = billing_info.zip_code
             form.cvv.data = billing_info.card_CVC
