e22ca1f39b59f22804a69d98df5b4d0629b5d1d4
Maksimilijan Marošević <mm51593@fer.hr>
2021-01-14 12:50:46 +0100

    Tip podataka za spremanje hashiranih lozinki promijenjen iz String u LargeBinary

--- app/database.py
+++ app/database.py
@@ -16,7 +16,7 @@ class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
     email = db.Column(db.String(50), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.LargeBinary(60), nullable=False)
 
     def getNotifications(self):
         return ModelNotification.query.filter_by(receiver_id=self.id, seen=False).all()
